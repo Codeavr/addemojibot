a8e3e5e087b2f4a2d5d4ee2ed8c8582726537fa8
Sergey Ankinovich <codeavr@ya.ru>
2019-04-22 19:34:00 +0300

    Fixed unused url

--- emoji_translate_api.py
+++ emoji_translate_api.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class API(object):
     def __init__(self, url='http://superemojitranslator.com/emoji-translate'):
         self._api_url = url
 
     def translate_to_emojies(self, text) -> str:
-        response = requests.post('http://superemojitranslator.com/emoji-translate',
-                                 data={'phrase-to-translate': text})
+        data = {'phrase-to-translate': text}
+        response = requests.post(self._api_url, data)
         parsed_html = BeautifulSoup(response.text, features='html5lib')
         translated_text = parsed_html.body.find(
             'textarea', attrs={'name': 'translated-phrase'}).text
