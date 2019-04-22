fcd85d5c460ac1e7048711bf2a81e2b6cfeb094b
Sergey Ankinovich <codeavr@ya.ru>
2019-04-22 18:41:06 +0300

    Fixed translation, added start command and text command

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from config import Config
 import emoji_translate_api
 import yandex_translate_api
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import Filters, Updater, InlineQueryHandler, CommandHandler, MessageHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineQueryResult
 
 config = Config()
@@ -15,41 +15,41 @@ latin_chars = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
 
 def all_chars_are_latin(text):
     for ch in text:
-        if ch not in latin_chars and not ch.isidentifier() and not ch.isdigit() and not ch.isspace():
+        if ch not in latin_chars and (ch.isidentifier() or ch.isdigit() or ch.isspace()):
             return False
     return True
 
 
-def on_inline_text(update, context):
-    query = context.inline_query.query
-    print('query:', query)
-    update.inline_query.answer([InlineQueryResultArticle(
-        id=uuid4(),
-        title="Caps",
-        input_message_content=query.upper())])
-
-    return
-
-    query = context.inline_query.query
-    if all_chars_are_latin(query):
-        eng_text = query
+def emojify(text) -> str:
+    if text.strip() == '':
+        return
+    if all_chars_are_latin(text):
+        eng_text = text
     else:
-        eng_text = ya_api.translate_text(query)
+        eng_text = ya_api.translate_text(text)
     emojies = emoji_api.translate_to_emojies(eng_text)
+    return emojies
 
+
+def on_inline_text(bot, update):
+    query = update.inline_query.query
+    emojies = emojify(query)
     results = [
         InlineQueryResultArticle(
             id=uuid4(),
             type='article',
-            title="Translated",
+            title=emojies,
             input_message_content=InputTextMessageContent(emojies))
     ]
     update.inline_query.answer(results)
 
 
-def start(update, context):
-    print('hi')
-    update.message.reply_text('Hi!')
+def start(bot, update):
+    update.message.reply_text('Type any text to add some emojies to it')
+
+
+def on_text_got(bot, update):
+    update.message.reply_text(emojify(update.message.text))
 
 
 def create_updater(tg_token, proxy=None):
@@ -65,8 +65,10 @@ if __name__ == "__main__":
     updater = create_updater(config.TG_TOKEN, config.PROXY)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, on_text_got))
+
     dispatcher.add_handler(InlineQueryHandler(on_inline_text))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                        level=logging.INFO)
     updater.start_polling()
     updater.idle()
